e24fc1b4084549294072a12cc6093b9b6db0a402
Borit-UTP <1420686@utp.edu.pe>
2017-10-09 01:09:36 -0500

    Creacion del proyecto
    Inicio del proyecto

---
+++ netgame/app/src/main/java/com/netgame/netgame/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.netgame.netgame.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.netgame.netgame.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
